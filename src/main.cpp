b88de6580385a5f59a2514f07ef7c948d138ea80
twobit <s8572327@gmail.com>
2019-04-25 22:08:15 +0200

    remove globals

--- src/main.cpp
+++ src/main.cpp
@@ -8,22 +8,119 @@
 #include <uv.h>
 
 
-struct Variable {
-    std::string name;
-    float       min;
-    float       max;
-    float       val;
-    bool        active;
+
+class App : public fx::App {
+public:
+    App(char* const path) : m_path(path) {}
+
+    void init() override {
+        gui::init();
+
+        m_vb = gfx::VertexBuffer::create(gfx::BufferHint::StaticDraw);
+        m_va = gfx::VertexArray::create();
+        m_va->set_primitive_type(gfx::PrimitiveType::TriangleStrip);
+        m_va->set_attribute(0, m_vb, gfx::ComponentType::Float, 2, false, 0, sizeof(glm::vec2));
+
+        std::vector<glm::vec2> v = {
+           { -1, -1 },
+           {  1, -1 },
+           { -1,  1 },
+           {  1,  1 },
+        };
+        m_vb->init_data(v);
+        m_va->set_count(v.size());
+
+        load_shader();
+
+        m_loop = uv_default_loop();
+        uv_fs_event_init(m_loop, &m_handle);
+        uv_fs_event_start(&m_handle, &event_callback, m_path, 0);
+    }
+
+    void free() override {
+        uv_loop_close(m_loop);
+        gui::free();
+        delete m_va;
+        delete m_vb;
+        delete m_shader;
+    }
+
+    void process_event(SDL_Event const& e) override {
+        gui::process_event(e);
+    }
+
+    void update() override {
+        uv_run(m_loop, UV_RUN_NOWAIT);
+        ++m_frame;
+
+        if (!m_shader) return;
+
+        gui::new_frame();
+        gui::set_next_window_pos({5, 5});
+        gui::begin_window("Variables");
+
+
+        for (Variable& v : m_variables) {
+            if (!v.active) continue;
+            gui::drag_float(v.name.c_str(), v.val, 1, v.min, v.max);
+            std::string u = "_" + v.name;
+            if (m_shader->has_uniform(u)) m_shader->set_uniform(u, v.val);
+        }
+
+        if (m_shader->has_uniform("iResolution")) {
+            m_shader->set_uniform("iResolution", glm::vec2(fx::screen_width(), fx::screen_height()));
+        }
+        if (m_shader->has_uniform("iFrame")) {
+            m_shader->set_uniform("iFrame", float(m_frame));
+        }
+        if (m_shader->has_uniform("iTime")) {
+            m_shader->set_uniform("iTime", (SDL_GetTicks() - m_start_time) * 0.001f);
+        }
+
+        gfx::clear({0, 0, 0, 1});
+        gfx::draw(m_rs, m_shader, m_va);
+        gui::render();
+    }
+
+
+private:
+
+    void load_shader();
+
+    static void event_callback(uv_fs_event_t* handle, const char*, int events, int) {
+        App* a = (App*) handle->data;
+        if (events & UV_CHANGE) {
+            uv_fs_event_stop(handle);
+            a->load_shader();
+            uv_fs_event_start(handle, &event_callback, a->m_path, 0);
+        }
+    }
+
+
+    struct Variable {
+        std::string name;
+        float       min;
+        float       max;
+        float       val;
+        bool        active;
+    };
+
+    char const*           m_path;
+    uv_loop_t*            m_loop;
+    uv_fs_event_t         m_handle{.data = this};
+    std::vector<Variable> m_variables;
+    gfx::Shader*          m_shader = nullptr;
+
+    uint32_t m_start_time = SDL_GetTicks();
+    uint32_t m_frame      = 0;
+
+    gfx::RenderState   m_rs;
+    gfx::VertexArray*  m_va = nullptr;
+    gfx::VertexBuffer* m_vb = nullptr;
 };
 
-std::vector<Variable> m_variables;
-gfx::Shader*          m_shader = nullptr;
-char const*           m_path;
 
-
-
-
-void load_shader() {
+void App::load_shader() {
     printf("loading shader...\n");
 
     std::ifstream file(m_path);
@@ -135,108 +232,11 @@ void load_shader() {
 }
 
 
-void event_callback(uv_fs_event_t* handle, const char*, int events, int) {
-    if (events & UV_CHANGE) {
-        uv_fs_event_stop(handle);
-        load_shader();
-        uv_fs_event_start(handle, &event_callback, m_path, 0);
-    }
-}
-
-
-class App : public fx::App {
-public:
-
-    void init() override {
-        gui::init();
-
-        m_vb = gfx::VertexBuffer::create(gfx::BufferHint::StaticDraw);
-        m_va = gfx::VertexArray::create();
-        m_va->set_primitive_type(gfx::PrimitiveType::TriangleStrip);
-        m_va->set_attribute(0, m_vb, gfx::ComponentType::Float, 2, false, 0, sizeof(glm::vec2));
-
-        std::vector<glm::vec2> v = {
-           { -1, -1 },
-           {  1, -1 },
-           { -1,  1 },
-           {  1,  1 },
-        };
-        m_vb->init_data(v);
-        m_va->set_count(v.size());
-
-        load_shader();
-
-        m_loop = uv_default_loop();
-        uv_fs_event_init(m_loop, &m_handle);
-        uv_fs_event_start(&m_handle, &event_callback, m_path, 0);
-    }
-
-    void free() override {
-        uv_loop_close(m_loop);
-        gui::free();
-        delete m_va;
-        delete m_vb;
-        delete m_shader;
-    }
-
-    void process_event(SDL_Event const& e) override {
-        gui::process_event(e);
-    }
-
-    void update() override {
-        uv_run(m_loop, UV_RUN_NOWAIT);
-        ++m_frame;
-
-        if (!m_shader) return;
-
-        gui::new_frame();
-        gui::set_next_window_pos({5, 5});
-        gui::begin_window("Variables");
-
-
-        for (Variable& v : m_variables) {
-            if (!v.active) continue;
-            gui::drag_float(v.name.c_str(), v.val, 1, v.min, v.max);
-            std::string u = "_" + v.name;
-            if (m_shader->has_uniform(u)) m_shader->set_uniform(u, v.val);
-        }
-
-        if (m_shader->has_uniform("iResolution")) {
-            m_shader->set_uniform("iResolution", glm::vec2(fx::screen_width(), fx::screen_height()));
-        }
-        if (m_shader->has_uniform("iFrame")) {
-            m_shader->set_uniform("iFrame", float(m_frame));
-        }
-        if (m_shader->has_uniform("iTime")) {
-            m_shader->set_uniform("iTime", (SDL_GetTicks() - m_start_time) * 0.001f);
-        }
-
-        gfx::clear({0, 0, 0, 1});
-        gfx::draw(m_rs, m_shader, m_va);
-        gui::render();
-    }
-
-
-private:
-
-    uint32_t m_start_time = SDL_GetTicks();
-    uint32_t m_frame = 0;
-
-    uv_loop_t*    m_loop;
-    uv_fs_event_t m_handle;
-
-    gfx::RenderState   m_rs;
-    gfx::VertexArray*  m_va = nullptr;
-    gfx::VertexBuffer* m_vb = nullptr;
-};
-
-
 int main(int argc, char** argv) {
     if (argc != 2) {
         printf("usage: %s shader_file\n", argv[0]);
         return 0;
     }
-    m_path = argv[1];
-    App a;
+    App a(argv[1]);
     return fx::run(a);
 }
